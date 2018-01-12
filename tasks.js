256796e953ff56153a860dcf701cac1b2cdef688
Alessa Messineo <apmessineo@gmail.com>
2018-01-12 15:37:05 -0500

    step 3- add prices

--- tasks.js
+++ tasks.js
@@ -35,7 +35,7 @@ const step1 = () => {
 }
 
 //         Step2 builds on step1 to add the work title.
-const step2= () => {
+const step2 = () => {
   let artistsAndTitles = [];
 
   crawlArr1.forEach((urlEnding) => {
@@ -66,6 +66,39 @@ const step2= () => {
   });
 }
 
+const step3 = () => {
+    let artistsWithPrices = [];
+
+  crawlArr1.forEach((urlEnding) => {
+    let pageToCrawl = `https://alessapm.github.io/lot1/${urlEnding}`;
+
+    request(pageToCrawl, (err, res, body) => {
+      if(err) {
+        console.log("Error: ", err);
+      }
+     if(res.statusCode === 200) {
+       let $ = cheerio.load(body);
+       let artist = $('h2').text();
+       let title = $('h3').text();
+       let price = $('div').last().text();
+       console.log('price: ', price)
+       let obj = {
+        artist: artist,
+        works: {title: title, price: price}
+       };
+       artistsWithPrices.push(obj);
+       if (artistsWithPrices.length === crawlArr1.length){
+        console.log('arr: ', artistsWithPrices);
+        return JSON.stringify(artistsWithPrices);
+       }
+     }
+
+    })
+
+  });
+}
+
 
 // step1();
-step2();
+// step2();
+step3();
